ace5d4f0b30dcdac7403785e5a7f65b46e90c067
Alex McLean <alex.mclean@hey.com>
2021-03-21 07:11:36 -0700

    Trying to override dropdown props

--- mui/src/App.tsx
+++ mui/src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  FormControl,
+  makeStyles,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles({
+  formControl: {
+    margin: 4,
+    minWidth: 120,
+    border: "0",
+  },
+  selectEmpty: {
+    marginTop: 4,
+  },
+  root: {
+    color: "red",
+    border: "1px solid black",
+    borderRadius: 6,
+  },
+  select: {
+    marginTop: "44px",
+    color: "green",
+    "& ul": {
+      backgroundColor: "#cccccc",
+    },
+    "& li": {
+      fontSize: 12,
+    },
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: any) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box p={6}>
+      <FormControl className={classes.formControl}>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          onChange={handleChange}
+          MenuProps={{
+            classes: { paper: classes.select },
+          }}
+          className={classes.root}
+          disableUnderline
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
   );
 }
 
